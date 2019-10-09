be2473bc7e42eb190eb18f3f3da4e68e9b3661b1
aaron blum <blumaa@gmail.com>
2019-10-09 09:57:43 -0500

    images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('%c HI', 'color: firebrick')
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(json => renderImages(json));
+});
+
+function renderImages(json) {
+  const imgContainer = document.getElementById('dog-image-container');
+  const imageURI = json.message;
+  for (let src of imageURI) {
+    let image = document.createElement('img');
+    image.src = src;
+    imgContainer.append(image);
+  };
+};

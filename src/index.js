b43beb990da5cc9cb6266c2a98662fd882742bc3
aaron blum <blumaa@gmail.com>
2019-10-09 11:54:35 -0500

    filtered breeds

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+const state = {};
+
+
 document.addEventListener('DOMContentLoaded', function() {
   console.log('%c HI', 'color: firebrick')
-  fetch('https://dog.ceo/api/breeds/image/random/4')
-    .then(resp => resp.json())
-    .then(json => renderImages(json));
+  // fetch('https://dog.ceo/api/breeds/image/random/4')
+  //   .then(resp => resp.json())
+  //   .then(json => renderImages(json));
 
     const breedUrl = 'https://dog.ceo/api/breeds/list/all';
     fetch(breedUrl)
       .then(resp => resp.json())
-      .then(json => renderBreeds(json));
+      .then(json => {
+
+        state.breeds = [...renderBreeds(json)]
+        console.log(state)
+        console.log(renderBreeds(json))
+
+      });
 
       const breed_ul = document.getElementById('dog-breeds');
-      console.log(breed_ul);
+
       breed_ul.addEventListener('click', function(event) {
         if (event.target.className === "breed") {
 
@@ -19,6 +28,23 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       });
 
+      // filter based on dropdown
+    const letterFilter = document.getElementById('breed-dropdown');
+    letterFilter.addEventListener('change', function(event) {
+      const breeds_ul = document.getElementById('dog-breeds');
+      const breedsList = document.getElementById('dog-breeds').children;
+      const letter = event.target.value
+      console.log(letter)
+      const activeBreeds = filterBreeds(letter, state.breeds)
+      breeds_ul.innerHTML = "";
+      activeBreeds.forEach( breed => {
+        console.log(breed);
+        if (activeBreeds.includes(breed)) {
+          breeds_ul.appendChild(breed);
+        }
+      })
+
+    })
 });
 
 
@@ -43,7 +69,7 @@ function renderBreeds(json) {
     for (let subtype of breeds[breed]) {
 
       breed_li = document.createElement('li');
-      breed_li.className = "breed";
+      breed_li.className = "breed " + breed;
       breed_li.textContent = subtype + " " + breed;
       breedsList.appendChild(breed_li);
     }
@@ -55,4 +81,18 @@ function renderBreeds(json) {
     breedsList.appendChild(breed_li);
   }
   };
+  return breedsList.children;
+}
+
+function filterBreeds(letter, listOfBreeds) {
+  const breedArr = Array.from(listOfBreeds)
+  console.log(breedArr)
+  let result =  breedArr.filter(breed => {
+    if (breed.classList.length > 1) {
+      return breed.classList[1][0] == letter;
+    } else {
+      return breed.textContent[0] == letter
+    }
+  });
+  return result;
 };
